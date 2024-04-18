a137153a60fcac97553ae7d04e3b38f6c8472e0c
jguidi1 <jguidi@ithaca.edu>
2024-04-18 10:23:53 -0400

    updated home logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,22 @@
-import HomeIcon from "../images/home.png"
+import HomeIcon from "../images/Top 25 Showdown-logos_transparent.png"
 import { motion } from "framer-motion"
 
 export default function Home() {
 
     return (
         <div>
+            
             <motion.div
                 animate={{ y: 100 }}
                 transition={{ ease: "easeOut", duration: 2 }}
                 className="flex justify-center items-center"
             >
+            
+            
+            
 
             <img src={HomeIcon} alt="" style={{width: 500}}/>
+           
             </motion.div>
      
 
